05803436989ee4d1e8639877558499c71119d403
Nesim <nesim.engineer@gmail.com>
2024-04-19 09:13:51 -0700

    template exampe of settings

---
+++ tests/conf_settings_template.py
@@ -0,0 +1,11 @@
+"""
+ this is  template of example keys to use
+ copy the template and fill these values into conf_settings.py
+ the values do not have to be hardcoded, 
+ they can be taken from the environment
+"""
+import os
+community_url="https://testabcd123456789.pubpub.org"
+community_id="fce230e8-c211-40e1-8a34-60a65c1aad08"
+password=os.environ("PASSWORD")
+email="nesim.engineer@gmail.com"
