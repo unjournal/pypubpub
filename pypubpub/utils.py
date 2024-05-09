a76e74ca67b6f16c77f6f46006a4a86151ae274b
Nesim <nesim.engineer@gmail.com>
2024-05-09 10:03:11 -0700

    utility to slugify a sentence

--- pypubpub/utils.py
+++ pypubpub/utils.py
@@ -23,3 +23,48 @@ def retry(sleep=2, retry=3):
 
 def generate_random_number_string(length):
     return ''.join(random.choice(string.digits) for _ in range(length))
+
+
+#https://www.phind.com/search?cache=dreok6n9yhkai8qqg18mpvbe
+import re
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from slugify import slugify  #python-slugify
+
+# Download stop words if not already downloaded
+# nltk.download('stopwords')
+
+def generate_slug(title, keep_stop_words=False,slug_length=1):
+    """ Generate a slug from a title """
+    if(keep_stop_words):
+        return slugify(title)
+    # Tokenize the title into words
+    words = word_tokenize(title)
+    
+    # Remove stop words
+    stop_words = set(stopwords.words('english'))
+    filtered_words = [word for word in words if word.casefold() not in stop_words]
+    return slugify(filtered_words)
+    # len_f = len(filtered_words) - slug_length
+    # if(slug_length==1):
+    #     return random.choice(filtered_words)
+    # if(slug_length==0 or len_f<=0):
+    #     return slugify(filtered_words)
+    # if(len_f==0):
+    #     pass
+    # elif(len_f==1):
+    #     filtered_words = filtered_words[0:1]
+
+    # if(len(filtered_words) >= slug_length):
+    #     pass
+    # else:
+        
+    # # if(len(filtered_words)=<slug_length):
+    #     # return slugify(filtered_words)
+    # # Join the filtered words back into a string
+    # filtered_title = ' '.join(filtered_words)
+    
+    # # Convert the filtered title into a slug
+    # slug = slugify(filtered_title)
+    
+    # return slug
